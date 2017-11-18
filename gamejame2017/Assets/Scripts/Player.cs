e201b0a129861d4e231858baeb45085274e5a2e1
McCoshen <dam479@usask.ca>
2017-11-18 02:20:30 -0600

    movemebnt script

--- gamejame2017/Assets/Scripts/Player.cs
+++ gamejame2017/Assets/Scripts/Player.cs
@@ -3,14 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+////////////////////////////////////////////////////////////////////////////////
+//                                   TYPES                                    //
+////////////////////////////////////////////////////////////////////////////////
+    struct inputType{
+        public float X;
+        public float Y;
+        public bool attack;
+        public bool block;
+    }
+   
+    delegate inputType inputDelegate();
+    delegate IEnumerator attackDelegate();
 
-	// Use this for initialization
-	void Start () {
-		
+ //////////////////////////////////////////////////////////////////////////////
+ //                            INSTANCE VARIABLES                            //
+ //////////////////////////////////////////////////////////////////////////////
+    private inputDelegate inputFunc;
+    private attackDelegate attack;
+
+ //////////////////////////////////////////////////////////////////////////////
+ //                              MAIN METHODS                                //
+ //////////////////////////////////////////////////////////////////////////////
+    void Start () {
+        attack = basicAttack;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        inputType input =  inputFunc();
+        if (input.attack) {
+            StartCoroutine(attack());
+        }
+        else if (input.block) {
+            StartCoroutine(block());
+        }
 	}
+
+
+ //////////////////////////////////////////////////////////////////////////////
+ //                            ATTACK TYPE METHODS                           //
+ //////////////////////////////////////////////////////////////////////////////
+    private IEnumerator block(){
+        yield return 0;
+    }
+    private IEnumerator basicAttack() {
+        yield return 0;
+    }
 }
